4b3a6a8b2eb3e517f1db020cc6ee9054fb6e5360
nre <nre@ableton.com>
2018-01-17 18:18:03 +0100

    Cleanup: place static imports first

--- src/test/groovy/com/ableton/JenkinsMocksTest.groovy
+++ src/test/groovy/com/ableton/JenkinsMocksTest.groovy
@@ -1,13 +1,12 @@
 package com.ableton
 
-import com.lesfurets.jenkins.unit.BasePipelineTest
-import org.junit.Before
-import org.junit.Test
-
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertFalse
 import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Test
 
 
 /**
